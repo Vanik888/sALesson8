44d374a000ba9c9e65a6c641d5f043cec0238109
Vanik <vanik_888@mail.ru>
2014-03-08 20:50:40 +0400

    button  ok & button cancell, added logger

--- src/com/example/salesson8/MainActivity.java
+++ src/com/example/salesson8/MainActivity.java
@@ -2,27 +2,36 @@ package com.example.salesson8;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements View.OnClickListener {
+	private static final String myLogName = "myLog";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+//		String logName = "myLog";
+		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.activity_main);
-
+		Log.d(myLogName, "Find ok button" );
+		Button btnOk = (Button) findViewById(R.id.btnOk);
+		btnOk.setOnClickListener(this);
+		Log.d(myLogName, "Find cancell button" );
+		Button btnCancell = (Button) findViewById(R.id.btnCancell);
+		btnCancell.setOnClickListener(this);
 	}
-	public void btnOkClick(View view) {
+	public void onClick(View view) {
 		TextView txtView = (TextView) findViewById(R.id.txtView);
-		txtView.setText("OkButton Clicked");				
-	}
-	public void btnCancellClick(View view) {
-		TextView txtView = (TextView) findViewById(R.id.txtView);
-		txtView.setText("Cancell Button clicked");
+		if (view.getId() == R.id.btnOk) {
+			txtView.setText("OK clicked");
+			Log.d(myLogName, "OK clicked");
+		}
+		if (view.getId() == R.id.btnCancell) {
+			txtView.setText("Cancell Clicked");
+			Log.d(myLogName, "Cancell clicked");
+		}
 	}
 
 	@Override
