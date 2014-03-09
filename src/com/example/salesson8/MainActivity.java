77bcf17928da012691fc5af058f10913532f35d4
Vanik <vanik_888@mail.ru>
2014-03-09 21:25:20 +0400

    lesson24: activity lifecycle

--- src/com/example/salesson8/MainActivity.java
+++ src/com/example/salesson8/MainActivity.java
@@ -1,48 +1,66 @@
 package com.example.salesson8;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends Activity  {
-	 final String TAG = "States";
-	  
-	    /** Called when the activity is first created. */
-	    @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_main);
-	        Log.d(TAG, "MainActivity: onCreate()");
-	    }
-	    
-	  @Override
-	  protected void onStart() {
-	    super.onStart();
-	    Log.d(TAG, "MainActivity: onStart()");
-	  }
+public class MainActivity extends Activity implements View.OnClickListener {
+	private final static String loggerName = "myLogger";	
 
-	  @Override
-	  protected void onResume() {
-	    super.onResume();
-	    Log.d(TAG, "MainActivity: onResume()");
-	  }
+	public void onClick(View v) {
+		Log.d(loggerName, "all works");
+		Intent myIntent = new Intent(this, SecondActivity.class );
+		Log.d(loggerName, "starting second activity");
+		startActivity(myIntent);
+	}
+	@Override
+	protected void onCreate(Bundle bn) {
+		super.onCreate(bn);
+		setContentView(R.layout.activity_main);
+		Button btn = (Button) findViewById(R.id.btnSend);
+		btn.setOnClickListener(this);
+		
+		Log.d(loggerName, "MainActivity: onCreate");	
+		
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(loggerName, "MainActivity: onStart");
+	}
+	
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(loggerName, "MainActivity: onResume");
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(loggerName, "MainActivity: onPause");
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(loggerName, "MainActivity: onStop");
+	}
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(loggerName, "MainActiviy: onDestroy");
+	}
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Log.d(loggerName, "MainActivity: onRestart");
+	}
 
-	  @Override
-	  protected void onPause() {
-	    super.onPause();
-	    Log.d(TAG, "MainActivity: onPause()");
-	  }
-
-	  @Override
-	  protected void onStop() {
-	    super.onStop();
-	    Log.d(TAG, "MainActivity: onStop()");
-	  }
-	    
-	  @Override
-	  protected void onDestroy() {
-	    super.onDestroy();
-	    Log.d(TAG, "MainActivity: onDestroy()");
-	  }
 }
