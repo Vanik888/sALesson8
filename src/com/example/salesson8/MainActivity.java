135d3800be5af4c7a7e389032c3846962556031c
Vanik <vanik_888@mail.ru>
2014-03-09 12:56:57 +0400

    lesson23: lifecycle of activity - onCreate, onStart, onResume, onPause, onStop, onDestroy

--- src/com/example/salesson8/MainActivity.java
+++ src/com/example/salesson8/MainActivity.java
@@ -1,39 +1,48 @@
 package com.example.salesson8;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-	private static final String myLogName = "myLog";
-    private	Button btnSend;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-//		String logName = "myLog";
-		super.onCreate(savedInstanceState);		
-		setContentView(R.layout.activity_main);
-		btnSend = (Button) findViewById(R.id.btnSend);
-		btnSend.setOnClickListener(this);
-	}
-	public void onClick(View view) {
-		if (view.getId() == R.id.btnSend) {
-			Log.d(myLogName, "Button send clicked");
-			Intent myIntent = new Intent(this, SecondActivity.class);
-			startActivity(myIntent);			
-		}		
-		else return;		
-	}
+public class MainActivity extends Activity  {
+	 final String TAG = "States";
+	  
+	    /** Called when the activity is first created. */
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_main);
+	        Log.d(TAG, "MainActivity: onCreate()");
+	    }
+	    
+	  @Override
+	  protected void onStart() {
+	    super.onStart();
+	    Log.d(TAG, "MainActivity: onStart()");
+	  }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	  @Override
+	  protected void onResume() {
+	    super.onResume();
+	    Log.d(TAG, "MainActivity: onResume()");
+	  }
 
+	  @Override
+	  protected void onPause() {
+	    super.onPause();
+	    Log.d(TAG, "MainActivity: onPause()");
+	  }
+
+	  @Override
+	  protected void onStop() {
+	    super.onStop();
+	    Log.d(TAG, "MainActivity: onStop()");
+	  }
+	    
+	  @Override
+	  protected void onDestroy() {
+	    super.onDestroy();
+	    Log.d(TAG, "MainActivity: onDestroy()");
+	  }
 }
