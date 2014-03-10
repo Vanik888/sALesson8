6adea9074c659e4a75dabc830dfc489cf065d2ed
Vanik <vanik_888@mail.ru>
2014-03-10 16:56:46 +0400

    lesson49: scroll with BaseAdapter

--- src/com/example/salesson8/MainActivity.java
+++ src/com/example/salesson8/MainActivity.java
@@ -1,52 +1,42 @@
 package com.example.salesson8;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity{
-
-	private final String ATTRIBUTE_NAME_TXT = "text";
-	private final String ATTRIBUTE_NAME_CHECKED = "checked";
-	private final String ATTRIBUTE_NAME_IMAGE = "image";
-	private ListView lv;
+	private ArrayList<Product> productList = new ArrayList<Product>();
+	private MyBoxAdapter myAdapter;
 	
-	public void onCreate(Bundle bn) {
-		super.onCreate(bn);
+	@Override
+	public void onCreate(Bundle bnl) {
+		super.onCreate(bnl);
 		setContentView(R.layout.activity_main);
-		String[] arrTexts = {"hello", "main", "asfd", "hjklj", "loea"};
-		boolean[] arrCheckBox = { true, false, true, false, true};
-		int img = R.drawable.ic_launcher;
 		
-		ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>(
-				arrTexts.length);
-		Map<String, Object> m;
-		for (int i = 0 ; i < arrTexts.length; i++ ) {
-			m = new HashMap<String, Object>();
-			m.put(ATTRIBUTE_NAME_TXT, arrTexts[i]);
-			m.put(ATTRIBUTE_NAME_CHECKED, arrCheckBox[i]);
-			m.put(ATTRIBUTE_NAME_IMAGE, img);
-			data.add(m);
-		}
-		String[] from = { ATTRIBUTE_NAME_TXT, 
-					    ATTRIBUTE_NAME_CHECKED, 
-						ATTRIBUTE_NAME_IMAGE };
-		int[] to = { R.id.txtView, R.id.chBox, R.id.imageV};
+		fillData();
+		myAdapter = new MyBoxAdapter(this, productList);
 		
-		SimpleAdapter sAdapter = new SimpleAdapter(this, data, 
-								R.layout.item,
-								from, to);
-		lv = (ListView) findViewById(R.id.listView);
-		lv.setAdapter(sAdapter);
-		
-				
-		
+		ListView lvMain = (ListView) findViewById(R.id.lvMain);
+		lvMain.setAdapter(myAdapter);
 	}
-
+	public void fillData() {
+		for (int i = 0; i <= 20 ; i++) {
+			productList.add(new Product(R.drawable.ic_launcher, "Product" + i, i*1000, false));		
+		}
+	}
+	public void showResult(View v) {
+		String result = "Your ordered things:\n";
+		for(Product p : myAdapter.getBox()) {
+			if (p.getBox()) {
+				result += p.getName()+"\n";
+			}			
+		}
+		Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+	}
+	
 }
