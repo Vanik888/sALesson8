3de4f8f2debe695f9c2d57d5e0270c42edbb7ef8
Vanik <vanik_888@mail.ru>
2014-03-12 01:21:47 +0400

    lesson82: progressbar message(what, arg1, arg2)

--- gen/com/example/salesson8/R.java
+++ gen/com/example/salesson8/R.java
@@ -29,7 +29,7 @@ public final class R {
     public static final class id {
         public static final int action_settings=0x7f090003;
         public static final int btnConnect=0x7f090000;
-        public static final int pbConnect=0x7f090002;
+        public static final int pbDownload=0x7f090002;
         public static final int tvStatus=0x7f090001;
     }
     public static final class layout {
