bd62df79c45c853e3f06d4f63635f7778de674ee
Vanik <vanik_888@mail.ru>
2014-03-12 12:50:46 +0400

    lesson86: I have added logger

--- src/com/example/salesson8/MainActivity.java
+++ src/com/example/salesson8/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.salesson8;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity{	
+	private final String logger = "myLogger";
 	private TextView tvInfo;
 	private MyTask myTask;
 	public void onCreate(Bundle bn) {
@@ -15,6 +17,8 @@ public class MainActivity extends Activity{
 	public void onClick(View v) {
 		tvInfo = (TextView) findViewById(R.id.tvInfo);
 		myTask = new MyTask(tvInfo);
+		Log.d(logger, "Starting new AsyncTask");
 		myTask.execute();
+		
 	}
 }
