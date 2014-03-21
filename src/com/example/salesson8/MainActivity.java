567ba9a6ccd36aacc5d6699727647181e14c9ea6
Vanik <vanik_888@mail.ru>
2014-03-22 01:52:57 +0400

    not finished

--- src/com/example/salesson8/MainActivity.java
+++ src/com/example/salesson8/MainActivity.java
@@ -1,26 +1,32 @@
 package com.example.salesson8;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	private final String logger = "myLogger";
-	private final String[] arrString = {"asdfsd", "sfhj", "shjh", "kjlj"};
-	private TextView tvInfo;
-	private MyTask myTask;
-	@Override
-	protected void onCreate(Bundle bn) {
-		super.onCreate(bn);
-		setContentView(R.layout.activity_main);
-		tvInfo = (TextView) findViewById(R.id.tvInfo);
-		myTask = new MyTask(tvInfo);
-		
-	}
-	public void onClick(View v) {
-		Log.d(logger, "onClick clicked");
-		myTask.execute(arrString);
-	}
+    private Double var1;
+    private Double var2;
+    @Override
+    public void onCreate(Bundle saveInstantState) {
+        super.onCreate(saveInstantState);
+        setContentView(R.layout.activity_main);
+
+        EditText etVar1 = (EditText) findViewById(R.id.etVar1);
+        var1 = Double.parseDouble(etVar1.getText().toString());
+
+        EditText etVar2 = (EditText) findViewById(R.id.etVar2);
+        var2 = Double.parseDouble(etVar1.getText().toString());
+
+    }
+
+    public void onClick(View v) {
+        
+    }
+
+
 }
