8f088a5574e772ca480eca0e79fe64fb5e4902b0
Vanik <vanik_888@mail.ru>
2014-03-28 23:18:11 +0400

    lesson208: tabs in action bar

--- src/com/example/salesson8/MainActivity.java
+++ src/com/example/salesson8/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.salesson8;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,24 +12,40 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ActionBar.TabListener{
 
-    private static final String myLogger = "myLogger";
+    private final String myLogger = "myLogger";
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        ActionBar.Tab tab = actionBar.newTab();
+        tab.setText("first tab");
+        tab.setTabListener(this);
+        actionBar.addTab(tab);
+
+        ActionBar.Tab tab2 = actionBar.newTab();
+        tab2.setText("second tab");
+        tab2.setTabListener(this);
+        actionBar.addTab(tab2);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        Log.d(myLogger, "onTabSelected: "+ tab.getText());
     }
 
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        Log.d(myLogger, "onTabUnselected: " + tab.getText());
+    }
 
-
-
-
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        Log.d(myLogger, "onTabReselected: "+ tab.getText());
+    }
 }
